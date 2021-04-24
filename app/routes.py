4dc652e62da248267969772d3b828543a447870a
ndkhiem3010 <ndkhiem3010@gmail.com>
2021-04-25 03:28:50 +0800

    bug fixes

--- app/routes.py
+++ app/routes.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+# Home and index page
 @app.route('/')
 @app.route('/index')
 def home():
     return render_template('index.html')
 
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
         pass
 
+# Run with debug mode
 if __name__ == '__main__':
     app.run(debug=True)
