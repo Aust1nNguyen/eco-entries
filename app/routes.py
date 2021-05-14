522b209a78f74d3936ade867c45f7ae342ce4b45
ndkhiem3010 <ndkhiem3010@gmail.com>
2021-05-15 02:20:12 +0800

    modify route

--- app/routes.py
+++ app/routes.py
@@ -59,7 +59,7 @@ def sign_up():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 # Quiz view
 @app.route('/ds_quiz')
@@ -74,10 +74,13 @@ def courses():
 # Courses view
 @app.route('/content')
 def content():
+    if current_user.is_authenticated:
+        current_user.enroll_course()
     return render_template("content.html", title= "Content")
 
 # Dashboard view
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template("dashboard.html", title='Dashboard')
 
@@ -97,39 +100,3 @@ def surplus():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-
-# authent = Blueprint("authent", __name__)
-
-# @authent.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     # Form processing - check for POST request
-#     if form.validate_on_submit():
-#         flash('Login requested for user {}, remember_me={}'.format(
-#             form.username.data, form.remember_me.data))
-#         return redirect(url_for('index'))
-#     return render_template('login.html', title='Sign In', form=form)
-
-# @authent.route("/sign_up", methods=["GET", "POST"])
-# def sign_up():
-#     if request.method == "POST":
-#         email = request.form.get('email')
-#         username = request.form.get('username')
-#         password1 = request.form.get("password1")
-#         password2 = request.form.get("password2")
-
-#         if (re.search('\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b', email)) == None:
-#             flash('Email address is invalid.', category='error')
-#         elif len(email) < 4:
-#             flash("Please enter a valid email.", category="error")
-#         elif len(username) < 2:
-#             flash('Username must be greater than 1 character.', category='error')
-#         elif password1 != password2:
-#             flash('Passwords don\'t match.', category='error')
-#         elif len(password1) < 8:
-#             flash('Password must be at least 8 characters.', category='error')
-#         else:
-#             flash('Account created!', category='success')
-#     return render_template("sign_up.html")
