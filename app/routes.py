ddfb2d83da5ac1cc7f6fd403afc445c5dd1cd3e0
ndkhiem3010 <ndkhiem3010@gmail.com>
2021-05-16 09:32:38 +0800

    bug fixes

--- app/routes.py
+++ app/routes.py
@@ -83,7 +83,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-<<<<<<< HEAD
 # Dashboard view
 @app.route('/dashboard')
 @login_required
@@ -96,15 +95,12 @@ def dashboard():
 def profile():
     return render_template("profile.html", title= "Profile")
 
-=======
->>>>>>> fbcb2a91b06c76738f511d9904c2a0ef40d1345d
 
 # Courses view
 @app.route('/content')
 def content():
     return render_template("content.html", title= "Content")
 
-<<<<<<< HEAD
 @app.route('/ds')
 def ds():    
     if current_user.is_authenticated:
@@ -114,8 +110,6 @@ def ds():
 
     return render_template("ds.html", title= "Demand and Supply")
 
-=======
->>>>>>> fbcb2a91b06c76738f511d9904c2a0ef40d1345d
 @app.route("/elasticity")
 def elasticity():
     if current_user.is_authenticated:
@@ -133,45 +127,6 @@ def surplus():
         db.session.commit()
     
     return render_template("surplus.html", title= "Consumer and Producer Surplus")
-
-<<<<<<< HEAD
-=======
-
-
-
-# Dashboard view
-@app.route('/dashboard')
-def dashboard():
-    return render_template("dashboard.html", title='Dashboard')
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html", title= "Profile")
-
-
-
-
-# Quiz view
-@app.route('/quiz')
-def quiz():
-    return render_template('quiz.html', title='Quiz', form='quizForm')
-
-@app.route('/ds_quiz')
-def ds_quiz():
-    return render_template('ds_quiz.html', title='Quiz', form='quizForm')
-
-@app.route('/elasticity_quiz')
-def elasticity_quiz():
-    return render_template('elasticity_quiz.html', title='Quiz', form='quizForm')
-
-@app.route('/surplus_quiz')
-def surplus_quiz():
-    return render_template('surplus_quiz.html', title='Quiz', form='quizForm')
-
-# Run with debug mode
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> fbcb2a91b06c76738f511d9904c2a0ef40d1345d
 
 # Quiz view
 @app.route('/handle_quiz/<quizname>/<quizurl>')
